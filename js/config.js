737f0376c0c8911186659e42d6da36bc17bd9fb1
Robert Nusser <robert.nusser@qvik.fi>
2021-01-28 16:00:21 +0200

    Added cognito credentials

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_z8wtpXY3y', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '8un7a30gffgb868ta1cpjg742', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
